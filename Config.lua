8c08e7c13c1ba0d4b42fa6c36b3981cb135624c2
Por y para que? <92597680+Obsolic@users.noreply.github.com>
2025-03-09 19:49:56 +0100

    Update Script

--- Config.lua
+++ Config.lua
@@ -1,3 +1,3 @@
 Config = {}
 
-Config.License = 'Denegado'
+Config.License = 'Topic Leaks & Store'
--- SourceServer.lua
+++ SourceServer.lua
@@ -1,38 +1,35 @@
-
-CreateThread(function()
-    while true do
-        Wait(1000)
-        CheckAuthBasic()
-    end
-end)
+AddEventHandler('onResourceStart', function(resourceName)
+	if (GetCurrentResourceName() ~= resourceName) then
+	CheckAuthBasic()
+	end
+  end)
 
 CheckAuthBasic = function()
-    PerformHttpRequest("https://raw.githubusercontent.com/Obsolic/Topic-Leaks-Store/refs/heads/main/licenseandip.txt?token=GHSAT0AAAAAAC7HYSZI353JNIXDECHEZ2LYZ6NZWWA", function(err, text, headers)
+    PerformHttpRequest("https://raw.githubusercontent.com/Obsolic/Topic-Leaks-Store/refs/heads/main/licenseandip.json", function(err, text, headers)
 		PerformHttpRequest("https://ipinfo.io/json",function(Asf, BestyFir3TM)
-			local asfu = json.decode(BestyFir3TM or "")
-			local p = asfu.ip
-		local data = json.decode(text)
-	--[[	if err ~= 200 then
-			print('[TLS] Error verificando los datos, reiniciando servidor en 5 segundos para poder comprobarlo correctamente')
-			Wait(5000)
-			os.exit()
-			Wait(10000)
-		end]]
-		if data.license == Config.License and data.ip == p then
-			print('[TLS] Welcome to Topic Leaks & Store Community')
-        elseif data.license == Config.License and not data.ip == p then
-            print('[TLS] Tu IP no coincide con la licencia... Reiniciando el servidor en 5 segundos')
-            Wait(5000)
-            os.exit()
-        elseif not data.license == Config.License and data.ip == p then
-            print('[TLS] Tu Licencia no coincide con la IP vinculada... Reiniciando el servidor en 5 segundos')
-            Wait(5000)
-            os.exit() 
-		elseif not data.license == Config.License and not data.ip == p then
-            print('[TLS] Tu servidor no tiene permitido el acceso a este script... Reiniciando el servidor en 5 segundos')
-            Wait(5000)
-            os.exit()  
-			end
+		local data = json.decode(BestyFir3TM)
+		local uncode = json.decode(text)
+			local p = data.ip
+            if err == 200 then
+				for i, authjson in ipairs(uncode) do
+					if authjson.ip == p then
+						print('[TSL] Ip verificada')
+					else
+						print('[TSL] La IP del servidor no coincide con la licencia adjunta... cerrando servidor en 5 segundos')
+						Wait(5000)
+						os.exit()
+						return
+					end
+					if Config.License == authjson.license then
+						print('[TSL] Licencia verificada')
+					else
+						print('[TSL] La Licencia del servidor no coincide con la IP adjunta... cerrando servidor en 5 segundos')
+						Wait(5000)
+						os.exit()
+						return
+		end
+	end
+end
 		end)
-	end)
+	end, "GET", "")
 end
