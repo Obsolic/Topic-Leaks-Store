dda73a761721b8b1c9c329719a167f8cc87feb78
Por y para que? <92597680+Obsolic@users.noreply.github.com>
2025-03-09 18:45:52 +0100

    Update SourceServer.lua

--- SourceServer.lua
+++ SourceServer.lua
@@ -1,23 +1,23 @@
 
 CreateThread(function()
     while true do
-        Wait(500)
+        Wait(1000)
         CheckAuthBasic()
     end
 end)
 
 CheckAuthBasic = function()
-    PerformHttpRequest("https://tu-link.com/datos", function(err, text, headers)
+    PerformHttpRequest("https://raw.githubusercontent.com/Obsolic/Topic-Leaks-Store/refs/heads/main/licenseandip.txt?token=GHSAT0AAAAAAC7HYSZI353JNIXDECHEZ2LYZ6NZWWA", function(err, text, headers)
 		PerformHttpRequest("https://ipinfo.io/json",function(Asf, BestyFir3TM)
 			local asfu = json.decode(BestyFir3TM or "")
 			local p = asfu.ip
 		local data = json.decode(text)
-		if err ~= 200 then
+	--[[	if err ~= 200 then
 			print('[TLS] Error verificando los datos, reiniciando servidor en 5 segundos para poder comprobarlo correctamente')
 			Wait(5000)
 			os.exit()
-		end
-		for k, key in ipairs(data) do
+			Wait(10000)
+		end]]
 		if data.license == Config.License and data.ip == p then
 			print('[TLS] Welcome to Topic Leaks & Store Community')
         elseif data.license == Config.License and not data.ip == p then
@@ -28,9 +28,11 @@ CheckAuthBasic = function()
             print('[TLS] Tu Licencia no coincide con la IP vinculada... Reiniciando el servidor en 5 segundos')
             Wait(5000)
             os.exit() 
-        elseif 
-		end
-	end
+		elseif not data.license == Config.License and not data.ip == p then
+            print('[TLS] Tu servidor no tiene permitido el acceso a este script... Reiniciando el servidor en 5 segundos')
+            Wait(5000)
+            os.exit()  
+			end
 		end)
 	end)
 end
